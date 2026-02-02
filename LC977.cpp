696da8ee8481eb916323d1e93615ff0d5c8d6e81
AshwiniRathod <02fe22bcs023@kletech.ac.in>
2026-02-02 17:12:55 +0530

    two pointers easy

---
+++ LC977.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int len = nums.size();
+
+        for (int i = 0; i < len; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
